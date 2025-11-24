20af1e46aee8dc002820fe341f168ff64524df8b
Abhijith S <abhijith4389@gmail.com>
2025-11-24 09:54:21 +0530

    updated project

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll(".filter-buttons button");
 const cards = document.querySelectorAll(".project-card");
+const extraProjects = document.querySelector('.extra-projects');
+const showMoreBtn = document.getElementById('show-more-btn');
 
 buttons.forEach(btn => {
     btn.addEventListener("click", () => {
@@ -15,5 +17,20 @@ buttons.forEach(btn => {
                 card.classList.toggle("hide", !card.classList.contains(filter));
             }
         });
+
+        //hide extra projects when filter changes
+         extraProjects.classList.add('hide');
+        showMoreBtn.textContent = "Show More";
     });
 });
+
+showMoreBtn.addEventListener('click', () => {
+    if(extraProjects.classList.contains('hide')){
+        extraProjects.classList.remove('hide');
+        showMoreBtn.textContent = "Show Less";
+    } else {
+        extraProjects.classList.add('hide');
+        showMoreBtn.textContent = "Show More";
+    }
+});
+
